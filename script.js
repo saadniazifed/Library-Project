bfa9e5ef98acf929176b07bdcc7f9127fec5b5b4
Saad Niazi <saadniazifed@gmail.com>
2022-03-03 19:53:14 +0500

    I refactored my code to be more minimal and cleaner. Last version was way too clunky and a mess. Finished with 95% of the project so far. ToggleButton is left.

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+// Selection of DOM Elements
+const mainContent = document.querySelector(".main-content");
+const addBookBtn = document.querySelector("#addBookBtn");
+const openFormButton = document.querySelector(".openFormButton");
+const form = document.querySelector("#form");
+
+// Event Listeners
+addBookBtn.addEventListener("click", addBookToLibrary);
+openFormButton.addEventListener("click", formHidden_orVisible);
+
 const myLibrary = [];
 let nextBookId = 1;
 
-function Books(title, author, pages, read, id) {
+function Book(title, author, pages, read, id) {
   this.title = title;
   this.author = author;
   this.pages = pages;
@@ -9,83 +19,73 @@ function Books(title, author, pages, read, id) {
   this.id = id;
 }
 
-// Selecting the button and adding an event listener.
-document
-  .getElementById("addBooks-btn")
-  .addEventListener("click", addBookToLibrary);
-
-// Function to run when the button is clicked
 function addBookToLibrary() {
   title = document.getElementById("title").value;
   author = document.getElementById("author").value;
   pages = document.getElementById("pages").value;
   read = document.getElementById("pages_read").value;
 
-  let book = new Books(title, author, pages, read, nextBookId);
+  let book = new Book(title, author, pages, read, nextBookId);
   myLibrary.push(book);
   nextBookId++;
   createTable();
 }
 
-// A function which creates a table everytime the user inputs his values.
 function createTable() {
-  let html = "<table border='1|1'>";
+  let html = `<table border='1|1'>`;
 
-  html += `<tr>`;
-  html += `<th>  TITLE  </th>`;
-  html += `<th>  AUTHOR  </th>`;
-  html += `<th>  PAGES  </th>`;
-  html += `<th>  HAVE YOU READ IT?  </th>`;
-  html += `<th>  Remove Row </th>`;
+  html += `
+          <tr>
+            <th>  TITLE  </th>
+            <th>  AUTHOR  </th>
+            <th>  PAGES  </th>
+            <th>  HAVE YOU READ IT?  </th>
+            <th>  Remove Row </th>`;
   for (var i = 0; i < myLibrary.length; i++) {
-    html += `<tr data-id="${myLibrary[i].id}" id="removeRows">`;
-    html += `<td> ${myLibrary[i].title}  </td>`;
-    html += `<td> ${myLibrary[i].author}  </td>`;
-    html += `<td> ${myLibrary[i].pages}  </td>`;
-    html += `<td> ${myLibrary[i].read}  </td>`;
-    html += `<td> <button class=removeRowBtn id="${myLibrary[i].id}" onclick=removeRow(this.id)>Delete Row</button>  </td>`;
+    html += `
+            <tr data-id="${myLibrary[i].id}" id="removeRows">
+              <td> ${myLibrary[i].title}  </td>
+              <td> ${myLibrary[i].author}  </td>
+              <td> ${myLibrary[i].pages}  </td>
+              <td id="fourth-row"> <button class="readStatus" id="${myLibrary[i].read}" onclick=changeStatus(this.id)> Read </button>  </td>
+              <td> <button class=removeRowBtn id="${myLibrary[i].id}" onclick=removeRow(this.id)>Delete Row</button>  </td>`;
     html += `</tr>`;
   }
 
   html += `</table>`;
-  document.getElementById("box").innerHTML = html;
+  mainContent.innerHTML = html;
 }
 
-// Related to opening and closing the form
-function openForm() {
-  document.getElementById("myForm").style.display = "block";
-}
-
-function closeForm() {
-  document.getElementById("myForm").style.display = "none";
-}
-
-document.querySelector(".button-53").addEventListener("click", openForm);
-document.querySelector("#cancelBooks-btn").addEventListener("click", closeForm);
-// Finished with opening and closing the form.
-
-// Function for finding the book ID.
 function getBookIndex(id) {
   return (index = myLibrary.findIndex((book) => {
     return book.nextBookId === id;
   }));
 }
 
-// Once the book ID has been found,add it in the function for removing the book object from the row
 function removeBookFromArray(id) {
-  //if the value returned in getBookIndex is less than 0, don't do anything.
   if (getBookIndex(id) < 0) {
     return;
-  }
-  //if the value returned is higher than 0, then based on that value, remove the object.
-  else myLibrary.splice(getBookIndex(id), 1);
+  } else myLibrary.splice(getBookIndex(id), 1);
 }
 
-// Creating a remove row button function
 function removeRow(id) {
-  //getting the parent node i.e the row of the Delete Row button
   const rowRemoval = document.getElementById(id).parentNode.parentNode;
   rowRemoval.remove();
-
   removeBookFromArray();
+}
+
+function changeStatus() {}
+
+function formHidden_orVisible() {
+  if (form.style.visibility == "visible") {
+    form.style.visibility = "hidden";
+  } else form.style.visibility = "visible";
+}
+
+function formVisible() {
+  form.style.visibility = "visible";
+}
+
+function formHidden() {
+  form.style.visibility = "hidden";
 }
