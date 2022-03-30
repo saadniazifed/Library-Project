04ee03d15b0a256728813bafb0e3d93bb3dd608f
Saad Niazi <saadniazifed@gmail.com>
2022-03-30 12:12:29 +0500

    Added ES6 Classes Functionality in the Library Project.

--- script.js
+++ script.js
@@ -13,12 +13,14 @@ openFormButton.addEventListener("click", formHidden_orVisible);
 const myLibrary = [];
 let nextBookId = 1;
 
-function Book(title, author, pages, read, id) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.id = id;
+class Book {
+  constructor(title, author, pages, read, id) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = id;
+  }
 }
 
 function addBookToLibrary() {
